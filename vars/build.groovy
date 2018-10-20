da5959d41e9ca4b119e7d504ac83b9f91366c726
Gabriel Wu <iamgabrielwu@gmail.com>
2018-10-20 08:43:01 +0800

    add update

--- vars/build.groovy
+++ vars/build.groovy
@@ -8,13 +8,9 @@ def call(body){
 }
 def sendMsgToSlack(){
       sh("""
-        curl -X POST 'https://hooks.slack.com/services/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' \
+        curl -X POST 'https://hooks.slack.com/services/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' \
         -H  'Content-type: application/json' \
         -H "Cache-Control: no-cache" \
-        -d @- <<EOF {
-          "title": "test",
-          "text":"this is dz.wu Testing"
-          }
-        EOF
+        -data '{"title": "test","text":"this is dz.wu Testing"}'
   """)
 }
